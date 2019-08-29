c57a6084428bf4950d51a65476557797e2b3140c
khaiknievel <khai@restless.org>
2019-08-29 16:12:10 +0800

    Update

--- build/precache-manifest.12c938cd8d89ec2e6ad417870fad65f0.js
+++ build/precache-manifest.12c938cd8d89ec2e6ad417870fad65f0.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
     "revision": "42ac5946195a7306e2a5",
-    "url": "/static/js/runtime~main.a8a9905a.js"
+    "url": "/react-webrtc/static/js/runtime~main.a8a9905a.js"
   },
   {
     "revision": "8717e9d511989d297e46",
-    "url": "/static/js/main.77c47a8a.chunk.js"
+    "url": "/react-webrtc/static/js/main.77c47a8a.chunk.js"
   },
   {
     "revision": "2fc46dc6377fc7887f50",
-    "url": "/static/js/2.3f6b389f.chunk.js"
+    "url": "/react-webrtc/static/js/2.3f6b389f.chunk.js"
   },
   {
     "revision": "8717e9d511989d297e46",
-    "url": "/static/css/main.85ef172f.chunk.css"
+    "url": "/react-webrtc/static/css/main.85ef172f.chunk.css"
   },
   {
     "revision": "6936e6e14069f955a5277ef19d1eca6f",
-    "url": "/index.html"
+    "url": "/react-webrtc/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12c938cd8d89ec2e6ad417870fad65f0.js"
+  "/react-webrtc/precache-manifest.12c938cd8d89ec2e6ad417870fad65f0.js"
 );
 
 workbox.clientsClaim();
@@ -29,6 +29,6 @@ workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
-  
+
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
